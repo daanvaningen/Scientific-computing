9d0036858a38fe15345c1a67b37bb3dcfee5ddd2
Sam Ferwerda <sam.ferwerda@hotmail.com>
2018-04-24 13:26:33 +0200

    Exercise G and H now compleet.

--- set1/jacobiIt.py
+++ set1/jacobiIt.py
@@ -5,13 +5,20 @@ from scipy.special import erfc
 import math
 
 class Grid:
-    def __init__(self, N, Epsilon, Max_Iterations, form):
+    def __init__(self, N, Epsilon, Max_Iterations, form, w=1):
         self.MaxIt = Max_Iterations         # stop condition
         self.N = N                          # Grid size
         self.Iterations = 0                 # No. of current iteration
         self.Epsilon = Epsilon              # Epsilon (when do we accept change to be neglected.)
         self.grid = np.zeros((N, N))        # Making grid of n x n
-        self.const = 1/4.0                  # Constant value for jacobi Iterations
+        self.w = w                          # The w value for SOR iteration
+        self.deltas = []                    # The convergence measures
+        
+        if form == "SOR":
+            self.const = w/4.0              # Value dependent on W
+        else:
+            self.const = 1/4.0              # Constant value for jacobi Iterations
+
         self.form  = form
 
     # The grid will update untill certain criteria are met.
@@ -21,32 +28,36 @@ class Grid:
             self.Iterations += 1
             tempgrid = np.zeros((self.N, self.N))
             Stable = True           # Unless determined otherwise
+            delta = 0               # check the largest convergence measure
             for i in range(self.N):
                 for j in range(self.N):
                     value = self.get_boundry_values(i, j, tempgrid)
                     tempgrid[i,j] = value
                     if abs(value - self.grid[i,j]) > self.Epsilon:  # Means not yet stable
                         Stable = False
+                        if abs(value - self.grid[i,j]) > delta:
+                            delta = abs(value - self.grid[i,j])
 
+            self.deltas.append(delta)
             self.grid = tempgrid
-        return self.grid, self.Iterations
+        return self.grid, self.deltas
 
     def get_boundry_values(self, i, j, tempgrid):
         # The values when we do a jacobi iteration
-        if self.form == "Gauss":
+        if self.form == "Gauss" or self.form == "SOR":
             if(j == 0 or j == self.N-1):
                 return j/(self.N-1)
             elif(i == 0):
-                return self.grid[0, j] + self.const*(self.grid[1, j] +
-                        self.grid[N-1, j] + self.grid[0, j+1] + tempgrid[0, j-1] - 4*self.grid[0,j])
+                return  self.const*(self.grid[1, j] +
+                        self.grid[N-1, j] + self.grid[0, j+1] + tempgrid[0, j-1]) + (1-self.w)*self.grid[i,j]
             elif(i == self.N-1):
-                return self.grid[i, j] + self.const*(self.grid[1, j] +
-                        tempgrid[i-1, j] + self.grid[i, j+1] + tempgrid[i, j-1] - 4*self.grid[i,j])
+                return self.const*(self.grid[1, j] +
+                        tempgrid[i-1, j] + self.grid[i, j+1] + tempgrid[i, j-1]) + (1-self.w)*self.grid[i,j]
             else:
-                return self.grid[i, j] + self.const*(self.grid[i+1, j] +
-                        tempgrid[i-1, j] + self.grid[i, j+1] + tempgrid[i, j-1] - 4*self.grid[i,j])
+                return self.const*(self.grid[i+1, j] +
+                        tempgrid[i-1, j] + self.grid[i, j+1] + tempgrid[i, j-1]) + (1-self.w)*self.grid[i,j]
 
-        # The values when we do Gauss iterations
+        # The values when we do jacobi iterations
         elif self.form == "Jac":
             if(j == 0 or j == self.N-1):
                 return j/(self.N-1)
@@ -73,7 +84,6 @@ def J():
     Equilibrium5 = Jacobi10_5.run()
     Equilibrium7 = Jacobi10_7.run()
 
-    print(Equilibrium3[1])
     # Iterations it took to obtain epsilon.
     epsilon10_3 = Equilibrium3[0][0]   # One vertical line representing changes in y
     epsilon10_5 = Equilibrium5[0][0]
@@ -115,7 +125,6 @@ def G():
     Equilibrium5 = Gauss10_5.run()
     Equilibrium7 = Gauss10_7.run()
 
-    print(Equilibrium3[1])
     # Iterations it took to obtain epsilon.
     epsilon10_3 = Equilibrium3[0][0]   # One vertical line representing changes in y
     epsilon10_5 = Equilibrium5[0][0]
@@ -144,16 +153,134 @@ def G():
     plt.colorbar()
     plt.show()
 
+def S():
+    # Grid creation for jacobi
+    SOR10_3 = Grid(N, 0, Max_Iterations, "SOR", w-0.5)
+    SOR10_5 = Grid(N, 0, Max_Iterations, "SOR", w)
+    SOR10_7 = Grid(N, 0, Max_Iterations, "SOR", w+0.5)
 
+    # Run the model
+    Equilibrium3 = SOR10_3.run()
+    Equilibrium5 = SOR10_5.run()
+    Equilibrium7 = SOR10_7.run()
+
+    epsilon10_3 = Equilibrium3[0][0]   # One vertical line representing changes in y
+    epsilon10_5 = Equilibrium5[0][0]
+    epsilon10_7 = Equilibrium7[0][0]
+
+    x = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    xticks = [i/float(N) for i in x]
+
+    # Show the change in y
+    yticks = [i/50 for i in range(50)]
+    plt.title("SOR iteration with max "+str(Max_Iterations)+ " iterations")
+    plt.plot(yticks, epsilon10_3, label = "w =" +str(round(w-0.5,1)))
+    plt.plot(yticks, epsilon10_5, label = "w =" +str(w))
+    plt.plot(yticks, epsilon10_7, label = "w =" +str(w+0.5))
+    plt.legend()
+    plt.xlabel("y")
+    plt.ylabel("Concentration c")
+    plt.show()
+
+def Compare():
+    SOR_ = Grid(N, 0, Max_Iterations, "SOR", w)
+    GAUSS_ = Grid(N, 0, Max_Iterations, "Gauss")
+    JACOBI_ = Grid(N, 0, Max_Iterations, "Jac")
+
+    # Run the model
+    Equilibrium3 = SOR_.run()
+    Equilibrium5 = GAUSS_.run()
+    Equilibrium7 = JACOBI_.run()
+
+    # One vertical line representing changes in y
+    Sor_change = Equilibrium3[0][0]
+    Gauss_change = Equilibrium5[0][0]
+    Jacobi_change = Equilibrium7[0][0]
+
+    x = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    xticks = [i/float(N) for i in x]
+
+    # Show the change in y
+    yticks = [i/50 for i in range(50)]
+    plt.title("Comparance with max " +str(Max_Iterations)+" iterations")
+    plt.plot(yticks, Sor_change, label = "SOR with w= " + str(w))
+    plt.plot(yticks, Gauss_change, label = "Gauss")
+    plt.plot(yticks, Jacobi_change, label = "Jacobi")
+    plt.legend()
+    plt.xlabel("y")
+    plt.ylabel("Concentration c")
+    plt.show()
+
+def Convergence_Measure(function = "Jac", w=1):
+    if function == "Jac":
+        jac = Grid(N, 0, Max_Iterations, "Jac")
+        data1 = jac.run()
+        delta_values1 = data1[1]
+
+        gauss = Grid(N, 0, Max_Iterations, "Gauss")
+        data2 = gauss.run()
+        delta_values2 = data2[1]
+        
+        iterations = [x for x in range(1, len(delta_values1)+1)]
+
+        plt.plot(iterations, delta_values1, label = "Jacobi")
+        plt.plot(iterations, delta_values2, label = "Gauss")
+        plt.title("Jacobi and Gauss convergence")
+        plt.legend()
+        plt.xlabel("Number of iterations k")
+        plt.ylabel("$\delta$")
+        plt.yscale('log')
+        plt.show()
+
+    else:
+        SOR1 = Grid(N, 0, Max_Iterations, "SOR", 0.5)
+        data1 = SOR1.run()
+        delta_values1 = data1[1]
+
+        SOR2 = Grid(N, 0, Max_Iterations, "SOR", 1.0)
+        data2 = SOR2.run()
+        delta_values2 = data2[1]
+
+        SOR3 = Grid(N, 0, Max_Iterations, "SOR", 1.5)
+        data3 = SOR3.run()
+        delta_values3 = data3[1]
+        
+        iterations = [x for x in range(1, len(delta_values1)+1)]
+
+        plt.plot(iterations, delta_values1, label = "w = 0.5")
+        plt.plot(iterations, delta_values2, label = "w = 1.0")
+        plt.plot(iterations, delta_values3, label = "w = 1.5")
+        plt.title("SOR convergence for different w")
+        plt.legend()
+        plt.xlabel("Number of iterations k")
+        plt.ylabel("$\delta$")
+        plt.yscale('log')
+        plt.show()
+        
 if __name__ == '__main__':
     # Initial conditions
     N = 50
-    Max_Iterations = 1000000000
-    Jacobi = True
-    Gauss = True
+    w = 1.7                        # For the SOR model
+    Max_Iterations = 500
+    Max_Iterations_SOR = 500        # Max iterations specifically for SOR
+    Jacobi = False                  # True if you want results, false if u want to skip
+    Gauss = False                   # True if you want results, false if u want to skip
+    SOR = False                     # True if you want results, false if u want to skip
+    Comparance = False
+    Convergence = True              
     
     if Jacobi:
         J()
 
     if Gauss:
         G()
+
+    if SOR:
+        S()
+
+    if Comparance:
+        Compare()
+
+    if Convergence:
+        Convergence_Measure()
+        Convergence_Measure(function = "SOR")
